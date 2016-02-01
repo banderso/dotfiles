dfbf79ffce78b41d34eaf7ee2ae6394c78b935e5
Ben Anderson <ben.anderson@nwea.org>
2016-01-31 16:34:21 -0800

    Add kanban story capture template

--- emacs_d/config/org-config.el
+++ emacs_d/config/org-config.el
@@ -4,7 +4,8 @@
 (setq org-capture-templates 
       '(("t" "Todo" entry (file+headline "~/org/journal.org" "Tasks") "* TODO %? %^g\n %i\n ")
         ("n" "Note" entry (file+headline "~/org/journal.org" "Notes") "* %? %^g\n %i\n ")
-        ("l" "Log"  entry (file+headline "~/org/journal.org" "Log")   "* %T - %? %^g\n %i\n " )))
+        ("l" "Log"  entry (file+headline "~/org/journal.org" "Log")   "* %T - %? %^g\n %i\n " )
+        ("s" "Story" entry (file+headline "~/org/kanban.org" "Stories") "** [#%^{priority|A|B|C|D}] %? %^g\n %i\n")))
 
 (setq org-log-done t)
 (setq org-agenda-files '("~/org/journal.org"))
